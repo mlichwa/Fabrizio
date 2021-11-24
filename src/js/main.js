028d21c7b15a00251c14dc97693b40b31ae3f598
Michal <michal@strangeflock.com>
2021-11-24 10:01:31 -0600

    styling fixes

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@
 
 $(function() {
     $(".toggle").on("click", function() {
+        $(".menu").toggleClass('active');
         if ($(".item").hasClass("active")) {
             $(".item").removeClass("active");
             $(this).find("a").html("<i class='fas fa-bars'></i>");
